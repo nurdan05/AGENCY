9441609ab65fa41bdd78c52057c041e356a5c5d4
Nurdan05 <baltabaevnurdan85@gmail.com>
2021-01-18 01:21:41 +0600

    made the whole site on a PC screen

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('.slider__wrapper').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    slidesToScroll: 1 ,
+});
+$('.features__wrapper').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
