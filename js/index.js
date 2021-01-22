ac4c0da355128769df795d620c8333fc3a63436d
Nurdan05 <baltabaevnurdan85@gmail.com>
2021-01-22 14:29:26 +0600

    added menu

--- js/index.js
+++ js/index.js
@@ -11,3 +11,17 @@ $('.features__wrapper').slick({
     // slidesToShow: 1,
     // slidesToScroll: 1,
   });
+  $('.clients__wrapper').slick({
+    dots: true,
+    arrows: false,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+  });
+
+let burger = document.querySelector('.burger__menu');
+let nav = document.querySelector('.nav');
+
+burger.addEventListener('click', function() {
+    burger.classList.toggle('is-active');
+    nav.classList.toggle('is-active');
+});
