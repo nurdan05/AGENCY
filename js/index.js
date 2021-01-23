157e6cccec649d38bc734ab2f6e12d80ffe2814b
Nurdan05 <baltabaevnurdan85@gmail.com>
2021-01-23 17:36:24 +0600

    change

--- js/index.js
+++ js/index.js
@@ -3,25 +3,25 @@ $('.slider__wrapper').slick({
     arrows: false,
     autoplay: true,
     autoplaySpeed: 1500,
-    slidesToScroll: 1 ,
+    slidesToScroll: 1,
 });
 $('.features__wrapper').slick({
     dots: true,
     arrows: false,
     // slidesToShow: 1,
     // slidesToScroll: 1,
-  });
-  $('.clients__wrapper').slick({
+});
+$('.clients__wrapper').slick({
     dots: true,
     arrows: false,
     // slidesToShow: 1,
+});
     // slidesToScroll: 1,
-  });
 
 let burger = document.querySelector('.burger__menu');
 let nav = document.querySelector('.nav');
 
-burger.addEventListener('click', function() {
+burger.addEventListener('click', function () {
     burger.classList.toggle('is-active');
     nav.classList.toggle('is-active');
 });
